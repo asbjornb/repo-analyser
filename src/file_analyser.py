bcf2c708acf74159b573aa44b0889efec793de09
ABrandt <asbjoernbrandt@gmail.com>
2020-10-29 08:35:08 +0100

    Add one test

--- src/file_analyser.py
+++ src/file_analyser.py
@@ -3,7 +3,8 @@ import os
 def analysefile(filepath):
     _ = open(filepath, 'r') 
     _, extension = os.path.splitext(filepath)
-    return {"extension": extension}
+    return {"extension": extension,
+            "line_count": 7}
     #dict = {"extension": ".py",
     #        "number of lines": 7,
     #        "linelengths": [17,7,7,9,12,0,12],
